5d7772050a7365c70d770f5c9747ceea80d061fc
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-05-07 23:18:09 -0300

    [ADD] stock_dropshipping_from_sale_order

---
+++ stock_dropshipping_from_sale_order/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2020 Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Stock Dropshipping From Sale Order',
+    'summary': """
+        Route for dropshipping from sale order.""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Escodoo,Odoo Community Association (OCA)',
+    'website': 'www.escodoo.com.br',
+    'depends': [
+        'sale',
+        'stock',
+        'stock_picking_sale_order_link',
+    ],
+    'data': [
+        'data/stock_data.xml',
+    ],
+    'demo': [
+    ],
+}
